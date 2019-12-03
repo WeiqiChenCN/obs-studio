e2e05a62ef054feeb01b32ab159f3070cdcea5a2
Weiqi Chen <weiqi_chen@outlook.com>
2019-12-03 22:44:35 +0800

    tmp-2019年12月5日。

--- UI/weiqi/weiqi-obs.cpp
+++ UI/weiqi/weiqi-obs.cpp
@@ -1,10 +1,11 @@
 #include "weiqi-obs.h"
 
+#include <obs.h>
+#include <obs.hpp>
+
 #include <util/base.h>
 #include <graphics/vec2.h>
 #include <media-io/audio-resampler.h>
-#include <obs.h>
-#include <obs.hpp>
 
 #include <intrin.h>
 
@@ -12,11 +13,20 @@
 
 
 #include <QDebug>
+#include <QLabel>
 #include <QTimer>
 #include <QtGlobal>
+#include <QCheckbox>
+#include <QCombobox>
+#include <QFormLayout>
 #include <QVBoxLayout>
+#include <QStandardItem>
+#include <QStandardItemModel>
 
 #include <iostream>
+
+#define QT_UTF8(str) QString::fromUtf8(str)
+#define QT_TO_UTF8(str) str.toUtf8().constData()
 
 static void init_global() {
 	if (!obs_startup("en-US", nullptr, nullptr))
@@ -103,11 +113,11 @@ static void add_source(obs_source_t* source, obs_scene_t* scene)
 weiqi_obs::weiqi_obs(QWidget* parent) :QWidget(parent) {
 	this->setMinimumSize(800, 600);
 
-	auto layout = new QVBoxLayout(this);
-	this->setLayout(layout);
+	myLayout = new QVBoxLayout(this);
+	this->setLayout(myLayout);
 	display = new OBSQTDisplay(this);
-	layout->addWidget(display);
-	connect(display, &OBSQTDisplay::DisplayResized, this, &weiqi_obs::displayResize);
+	myLayout->addWidget(display);
+	//connect(display, &OBSQTDisplay::DisplayResized, this, &weiqi_obs::displayResize);
 	QMetaObject::invokeMethod(this, "start_obs");
 };
 
@@ -129,11 +139,10 @@ void weiqi_obs::start_obs()
 		throw "obs_source_create window_capture failure...";
 	}
 	auto properties = obs_source_properties(source);
-	//obs_properties_destroy(properties);
 
 	auto property = obs_properties_first(properties);
 	std::cerr << "===============Properties===============\n";
-
+#if 0
 	while (property) {
 		const char* name = obs_property_name(property);
 		obs_property_type type = obs_property_get_type(property);
@@ -194,11 +203,12 @@ do{ \
 		case OBS_PROPERTY_GROUP:
 			HEAD();
 		}
-
-
 		obs_property_next(&property);
 	}
 
+#endif
+
+	obs_properties_destroy(properties);
 
 	obs_display_set_enabled(display->GetDisplay(), true);
 	obs_display_add_draw_callback(
@@ -216,6 +226,15 @@ do{ \
 	//obs_source_release(scene_source);
 	obs_source_release(source);
 	obs_scene_release(scene);
+
+	OBSData settings = obs_source_get_settings(source);
+	obs_data_release(settings);
+	myLayout->addWidget(new PropertiesView(
+		settings, source,
+		(PropertiesReloadCallback)obs_source_properties,
+		(PropertiesUpdateCallback)obs_source_update));
+
+	
 
 }
 
@@ -248,4 +267,213 @@ int main(int argc, char* argv[])
     weiqi_obs obs(nullptr);
     obs.show();
     return qapp.exec();
+}
+
+PropertiesView::PropertiesView(
+	OBSData settings_, void* obj_,
+	PropertiesReloadCallback reloadCallback, PropertiesUpdateCallback callback_)
+:	settings(settings_),
+	properties(nullptr, obs_properties_destroy),
+	reloadCallback(reloadCallback),
+	callback(callback_),
+	obj(obj_)
+
+{
+	if (obj) {
+		properties.reset(reloadCallback(obj));
+	}
+
+	uint32_t flags = obs_properties_get_flags(properties.get());
+	deferUpdate = (flags & OBS_PROPERTIES_DEFER_UPDATE) != 0;
+
+	reloadProperties();
+}
+
+void PropertiesView::reloadProperties()
+{
+	if (widget)
+		widget->deleteLater();
+	widget = new QWidget();
+	QFormLayout* layout = new QFormLayout;
+	layout->setFieldGrowthPolicy(QFormLayout::AllNonFixedFieldsGrow);
+	widget->setLayout(layout);
+
+	QSizePolicy mainPolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+	QSizePolicy policy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+	//widget->setSizePolicy(policy);
+
+	layout->setLabelAlignment(Qt::AlignRight);
+
+	obs_property_t* property = obs_properties_first(properties.get());
+	bool hasNoProperties = !property;
+
+	while (property) {
+		addProperty(property, layout);
+		obs_property_next(&property);
+	}
+
+}
+
+
+void PropertiesView::addProperty(
+	obs_property_t* property,
+	QFormLayout* layout)
+{
+	const char* name = obs_property_name(property);
+	obs_property_type type = obs_property_get_type(property);
+
+	if (!obs_property_visible(property))
+		return;
+
+	QLabel* label = nullptr;
+	QWidget* widget = nullptr;
+	bool warning = false;
+	switch (type) {
+	case OBS_PROPERTY_INVALID:
+		return;
+	case OBS_PROPERTY_BOOL:
+		widget = addCheckbox(property);
+		break;
+	case OBS_PROPERTY_LIST:
+		widget = addList(property);
+		break;
+	}
+
+	if (widget && !obs_property_enabled(property))
+		widget->setEnabled(false);
+
+	if (!label && type != OBS_PROPERTY_BOOL &&
+		type != OBS_PROPERTY_BUTTON && type != OBS_PROPERTY_GROUP)
+		label = new QLabel(obs_property_description(property));
+
+	if (warning && label) //TODO: select color based on background color
+		label->setStyleSheet("QLabel { color: red; }");
+	
+	/*if (label && minSize) {
+		label->setMinimumWidth(minSize);
+		label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+	}*/
+
+	if (label && !obs_property_enabled(property))
+		label->setEnabled(false);
+
+	if (!widget)
+		return;
+
+	layout->addRow(label, widget);
+
+}
+
+QWidget* PropertiesView::addCheckbox(obs_property_t* prop)
+{
+	const char* name = obs_property_name(prop);
+	const char* desc = obs_property_description(prop);
+	bool val = obs_data_get_bool(settings, name);
+
+	QCheckBox* checkbox = new QCheckBox(QT_UTF8(desc));
+	checkbox->setCheckState(val ? Qt::Checked : Qt::Unchecked);
+	return widget;
+}
+
+QWidget* PropertiesView::addList(obs_property_t* prop)
+{
+	const char* name = obs_property_name(prop);
+	QComboBox* combo = new QComboBox();
+	obs_combo_type type = obs_property_list_type(prop);
+	obs_combo_format format = obs_property_list_format(prop);
+	size_t count = obs_property_list_item_count(prop);
+	int idx = -1;
+
+	for (size_t i = 0; i < count; i++)
+		addComboItem(combo, prop, format, i);
+
+	if (type == OBS_COMBO_TYPE_EDITABLE)
+		combo->setEditable(true);
+
+	combo->setMaxVisibleItems(40);
+	combo->setToolTip(QT_UTF8(obs_property_long_description(prop)));
+#if 0
+	string value = from_obs_data(settings, name, format);
+
+	if (format == OBS_COMBO_FORMAT_STRING &&
+		type == OBS_COMBO_TYPE_EDITABLE) {
+		combo->lineEdit()->setText(QT_UTF8(value.c_str()));
+	}
+	else {
+		idx = combo->findData(QByteArray(value.c_str()));
+	}
+
+	if (type == OBS_COMBO_TYPE_EDITABLE)
+		return NewWidget(prop, combo,
+			SIGNAL(editTextChanged(const QString&)));
+
+	if (idx != -1)
+		combo->setCurrentIndex(idx);
+
+	if (obs_data_has_autoselect_value(settings, name)) {
+		string autoselect =
+			from_obs_data_autoselect(settings, name, format);
+		int id = combo->findData(QT_UTF8(autoselect.c_str()));
+
+		if (id != -1 && id != idx) {
+			QString actual = combo->itemText(id);
+			QString selected = combo->itemText(idx);
+			QString combined = QTStr(
+				"Basic.PropertiesWindow.AutoSelectFormat");
+			combo->setItemText(idx,
+				combined.arg(selected).arg(actual));
+		}
+	}
+
+	QAbstractItemModel* model = combo->model();
+	warning = idx != -1 &&
+		model->flags(model->index(idx, 0)) == Qt::NoItemFlags;
+
+	WidgetInfo* info = new WidgetInfo(this, prop, combo);
+	connect(combo, SIGNAL(currentIndexChanged(int)), info,
+		SLOT(ControlChanged()));
+	children.emplace_back(info);
+
+	/* trigger a settings update if the index was not found */
+	if (idx == -1)
+		info->ControlChanged();
+#endif
+	return combo;
+}
+
+void PropertiesView::addComboItem(QComboBox* combo, obs_property_t* prop, obs_combo_format format, size_t idx)
+{
+	const char* name = obs_property_list_item_name(prop, idx);
+	QVariant var;
+
+	if (format == OBS_COMBO_FORMAT_INT) {
+		long long val = obs_property_list_item_int(prop, idx);
+		var = QVariant::fromValue<long long>(val);
+
+	}
+	else if (format == OBS_COMBO_FORMAT_FLOAT) {
+		double val = obs_property_list_item_float(prop, idx);
+		var = QVariant::fromValue<double>(val);
+
+	}
+	else if (format == OBS_COMBO_FORMAT_STRING) {
+		var = QByteArray(obs_property_list_item_string(prop, idx));
+	}
+
+	combo->addItem(QT_UTF8(name), var);
+
+	if (!obs_property_list_item_disabled(prop, idx))
+		return;
+
+	int index = combo->findText(QT_UTF8(name));
+	if (index < 0)
+		return;
+
+	QStandardItemModel* model =
+		dynamic_cast<QStandardItemModel*>(combo->model());
+	if (!model)
+		return;
+
+	QStandardItem* item = model->item(index);
+	item->setFlags(Qt::NoItemFlags);
 }
--- UI/weiqi/weiqi-obs.h
+++ UI/weiqi/weiqi-obs.h
@@ -5,6 +5,8 @@
 #include <qt-display.hpp>
 
 #include <QWidget>
+#include <QComboBox>
+#include <QFormLayout>
 #include <QMetaObject>
 #include <QApplication>
 
@@ -25,5 +27,40 @@ protected:
 	obs_source_t* scene_source;
 	obs_data_t* window_capture_settings;
 	OBSQTDisplay* display;
+	QWidget* view;
+	QLayout* myLayout;
+};
+
+typedef obs_properties_t* (*PropertiesReloadCallback)(void* obj);
+typedef void (*PropertiesUpdateCallback)(void* obj, obs_data_t* settings);
+
+class PropertiesView : public QWidget {
+	Q_OBJECT
+	using properties_delete_t = decltype(&obs_properties_destroy);
+	using properties_t =
+		std::unique_ptr<obs_properties_t, properties_delete_t>;
+public:
+	PropertiesView(	OBSData settings_, void* obj_,
+			PropertiesReloadCallback reloadCallback,
+			PropertiesUpdateCallback callback_);
+public slots:
+	void reloadProperties();
+private:
+	void addProperty(obs_property_t* property, QFormLayout* layout);
+	QWidget* addCheckbox(obs_property_t* prop);
+	QWidget* addList(obs_property_t* prop);
+	QWidget* newWidget(obs_property_t* prop, QWidget* widget,
+		const char* signal);
+	void addComboItem(QComboBox* combo, obs_property_t* prop,
+		obs_combo_format format, size_t idx);
+private:
+	OBSData settings;
+	void* obj = nullptr;
+	QWidget* widget = nullptr;
+	properties_t properties;
+	PropertiesReloadCallback reloadCallback;
+	PropertiesUpdateCallback callback = nullptr;
+	bool deferUpdate;
+
 };
 
